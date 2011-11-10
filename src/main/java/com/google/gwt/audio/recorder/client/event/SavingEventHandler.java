f253a9d74d64baba14e8fec85e71622b820bc193
Pierre Merienne <pierre.merienne@gmail.com>
2011-11-10 18:14:48 +0100

    Ajout d'une UI

--- src/main/java/com/google/gwt/audio/recorder/client/event/SavingEventHandler.java
+++
@@ -1,8 +0,0 @@
-package com.google.gwt.audio.recorder.client.event;
-
-import com.google.gwt.event.shared.EventHandler;
-
-public interface SavingEventHandler extends EventHandler {
-
-	void onSavingEvent(SavingEventEvent event);
-}
